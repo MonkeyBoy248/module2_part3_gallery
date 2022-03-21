24b8e36b41f2ebb727fc5eebe401cd38c75865ac
Kirill Temnyi <kirik201100@mail.ru>
2022-03-21 15:27:34 +0300

    refactor: create authorized users list for authentication module
    MODULE-2-PART-3

--- backend/src/authorized_users_list.ts
+++ backend/src/authorized_users_list.ts
@@ -1,6 +1,6 @@
-import {AuthorizedUser} from "./interfaces/authorizedUser";
+import {Authorized_user} from "./interfaces/authorized_user";
 
-export const authorized_users: AuthorizedUser = {
+export const authorized_users: Authorized_user = {
   'asergeev@flo.team':'jgF5tn4F',
   'vkotikov@flo.team':'po3FGas8',
   'tpupkin@flo.team':'tpupkin@flo.team'
