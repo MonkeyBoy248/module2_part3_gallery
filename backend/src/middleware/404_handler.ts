6ebeb2de8d6843d0bd3a22bbbfe1316e83d2ff26
Kirill Temnyi <kirik201100@mail.ru>
2022-03-25 18:27:59 +0300

    refactor: change the path to the 404 page file from absolute to relative
    MODULE-2-PART-3

--- backend/src/middleware/404_handler.ts
+++ backend/src/middleware/404_handler.ts
@@ -1,12 +1,11 @@
 import * as express from "express";
 import { Request , Response, NextFunction} from "express";
+import path from "path";
 
 
 export function nonexistentPageHandler (req: Request, res: Response, next: NextFunction) {
-  res.status(404);
-
   if (req.accepts('html')) {
-    res.sendFile('/Users/user/projects/module2/module2_part3_gallery/backend/views/pages/404.html');
+    res.status(404).sendFile(path.join( __dirname, '..', '..', 'views', 'pages', '404.html'));
     return;
   }
 }
