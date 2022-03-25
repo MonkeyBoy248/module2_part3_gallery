5f5008e85dc09af5490a36b8f27f1e16d100d120
Kirill Temnyi <kirik201100@mail.ru>
2022-03-25 18:33:01 +0300

    chore: change destination folder path
    MODULE-2-PART-3

--- backend/src/middleware/picture_upload.ts
+++ backend/src/middleware/picture_upload.ts
@@ -2,6 +2,5 @@ import multer from "multer";
 import path from "path";
 import { Request, Response, NextFunction } from "express";
 
-
-export const upload = multer({dest: path.join(__dirname, '..', '..', 'backend', 'public', 'api_images')});
+export const upload = multer({dest: path.join(__dirname, '..', '..', 'public', 'api_images')});
 
