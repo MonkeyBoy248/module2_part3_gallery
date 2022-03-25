3f6f1ba0d0a620c7a9f01645aa9e66f7c54eb47e
Kirill Temnyi <kirik201100@mail.ru>
2022-03-25 18:30:45 +0300

    chore: replace res.status(codeNumber).send() with res.sendStatus()
    MODULE-2-PART-3

--- backend/src/middleware/authentication.ts
+++ backend/src/middleware/authentication.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export function checkAuthorizationHeader (req: Request, res: Response, next: NextFunction) {
   if (req.headers.authorization !== 'token') {
-    res.status(403).send();
+    res.sendStatus(403)
     return;
   }
 
