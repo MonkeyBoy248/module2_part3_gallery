255c37f0f91ddb9a0f74f701d9c4d2c4ba404ee9
Kirill Temnyi <kirik201100@mail.ru>
2022-03-29 12:28:24 +0300

    refactor: change logs date format
    add timezone offset for date in ISO format. Remove or replace unnecessary symbols
    
    MODULE-2-PART-3

--- backend/src/utils/log_format.ts
+++ backend/src/utils/log_format.ts
@@ -1,19 +1,25 @@
 import fs from "fs";
 import path from "path";
+import {paths} from "../../config";
 
 let fileName = setDateFormat();
 
 export function setDateFormat ()  {
-  const currentDate = new Date();
-  return currentDate.toISOString();
+  const timeZoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const currentDate = Date.now() - timeZoneOffset
+  const dateAsISO = new Date(currentDate).toISOString();
+  const formattedISO = dateAsISO.replace('T', ' ')
+    .slice(0, dateAsISO.lastIndexOf('.'));
+
+  return formattedISO;
 }
 
 setInterval(() => {
   fileName = setDateFormat();
-}, 30000)
+}, 3600000)
 
 function openNewFileStream (data: string) {
-  const stream = fs.createWriteStream (path.join('logs', fileName), {flags: 'a'});
+  const stream = fs.createWriteStream (path.join(paths.LOGS_PATH, fileName), {flags: 'a'});
   stream.write(data + '\n');
 }
 
