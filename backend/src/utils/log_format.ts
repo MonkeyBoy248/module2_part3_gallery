df18c757793099c548878f6697ebca79a2db659e
Kirill Temnyi <kirik201100@mail.ru>
2022-03-31 11:46:14 +0300

    refactor: remove custom date format
    in setDateFormat function remove custom date formatting with timezone offset. Only date.toISOString left
    
    MODULE-2-PART-3

--- backend/src/utils/log_format.ts
+++ backend/src/utils/log_format.ts
@@ -5,12 +5,9 @@ import {paths} from "../config";
 let fileName = setDateFormat();
 
 export function setDateFormat ()  {
-  const timeZoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
-  const currentDate = Date.now() - timeZoneOffset
-  const dateAsISO = new Date(currentDate).toISOString();
+  const date = new Date();
 
-  return dateAsISO.replace('T', ' ')
-    .slice(0, dateAsISO.lastIndexOf('.'));
+  return date.toISOString();
 }
 
 setInterval(() => {
